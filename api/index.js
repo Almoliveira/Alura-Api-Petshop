c448c9e701578d5ba4b5f1c2a2ea1555fb5c99fb
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-12-30 05:11:32 -0300

    Aula 2 Finalizada

--- api/index.js
+++ api/index.js
@@ -3,9 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const config = require('config')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
 
-app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!\n\n\n'))
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,11 +1,56 @@
-const roteador = require('express').Router()
-const TabelaFornecedor = require('./TabelaFornecedor')
+const roteador = require("express").Router();
+const TabelaFornecedor = require("./TabelaFornecedor");
+const Fornecedor = require('./Fornecedor');
 
-roteador.use('/', async (requisicao, resposta) => {
-    const resultados = await TabelaFornecedor.listar()
+roteador.get("/", async (req, res) => {
+  const resultados = await TabelaFornecedor.listar();
+  res.send(JSON.stringify(resultados));
+});
+
+roteador.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
     resposta.send(
-        JSON.stringify(resultados)
+        JSON.stringify(fornecedor)
     )
 })
 
-module.exports = roteador
+
+roteador.get('/:idFornecedor', async(req,res) => {
+    
+
+    try{
+        const id = req.params.idFornecedor;
+        const fornecedor = new Fornecedor({ id: id,  })
+        await fornecedor.carregar();
+        res.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (er) {
+        res.send(
+            JSON.stringify({
+                mensagem: er.message
+            })
+        )
+    }
+});
+
+roteador.put('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const dadosRecebidos = req.body
+        const dados = Object.assign({}, dadosRecebidos, { id: id })
+        const fornecedor = new Fornecedor(dados)
+        await fornecedor.atualizar()
+        res.end()
+    } catch (erro) {
+        res.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
+})
+
+module.exports = roteador;
